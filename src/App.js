b63268509f56c1964daf9e00668921b4d02daf3f
Liam Morrison <liammorrison11@gmail.com>
2021-04-27 11:12:42 -0500

    Inital commit for recipeApp

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import Recipe from "./Recipe";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const APP_ID = "e37416c1";
+  const APP_KEY = "c2a677df1e7fa9edfabfb82342adbb76";
+
+  const [recipes, setRecipes] = useState([]);
+  const [search, setSearch] = useState("");
+  const [query, setQuery] = useState("chicken");
+
+  // useEffect used with a callback allows us to run our API query once the query(search once search button is pressed) has been completed.
+  useEffect(() => {
+    getRecipes();
+  }, [query]);
+
+  // async await used to fetch data from edamam API.
+  const getRecipes = async () => {
+    const response = await fetch(
+      `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`
+    );
+    // always add await when fetching data from a third party.
+    const data = await response.json();
+    setRecipes(data.hits);
+    console.log(data.hits);
+  };
+
+  // allows us to use event listener to upate our search value.
+  // doing this allows us to update our search state in real time
+  // while prepping another const to accept our search when it is completed via the search button.
+  const updateSearch = (e) => {
+    setSearch(e.target.value);
+    // console.log(search);
+  };
+
+  // function to prevent submit from refreshing page on click. (form submit button will refresh page by default when clicked.)
+  // will set our query to the search when the search button is clicked.
+  // will then reset the text in search bar to be blank.
+  const getSearch = (e) => {
+    e.preventDefault();
+    setQuery(search);
+    setSearch("");
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={getSearch} className="search-form">
+        <input
+          className="search-bar"
+          type="text"
+          value={search}
+          onChange={updateSearch}
+        />
+        <button className="search-button" type="submit">
+          Search
+        </button>
+      </form>
+      <div className="recipes">
+        {recipes.map((recipe) => (
+          <Recipe
+            key={recipe.recipe.label}
+            title={recipe.recipe.label}
+            calories={recipe.recipe.calories}
+            image={recipe.recipe.image}
+            ingredients={recipe.recipe.ingredients}
+          />
+        ))}
+      </div>
     </div>
   );
-}
+};
 
 export default App;
